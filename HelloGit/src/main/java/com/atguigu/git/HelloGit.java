a8e1e17c8a9d0b15f13d27ee06d79912e474b57c
username <useremail>
2020-10-29 10:55:51 +0800

    主干添加

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -5,5 +5,7 @@ public class HelloGit {
         System.out.println("hell git");
         System.out.println("更新1");
         System.out.println("创建分hi");
+        System.out.println("主干添加");
+
     }
 }
